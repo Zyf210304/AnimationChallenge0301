03192ce3d64ff082406cf5faa1aad067dd57528a
张亚飞 <672233580@qq.com>
2023-03-01 14:26:35 +0800

    Initial Commit

---
+++ AnimationChallenge0301/AnimationChallenge0301App.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationChallenge0301App.swift
+//  AnimationChallenge0301
+//
+//  Created by 张亚飞 on 2023/3/1.
+//
+
+import SwiftUI
+
+@main
+struct AnimationChallenge0301App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
